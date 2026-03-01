06640e3262259656fd128b0e4682d9a962dd4ce0
sbalbuenaSeat <saray.balbuena@code.seat>
2026-03-01 15:25:54 +0100

    feat: adding create and finAll function entry service

--- src/entries/entries.service.ts
+++ src/entries/entries.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Entry, EntryDocument } from './schemas/entry.schema';
+import { Model } from 'mongoose';
+import { CreateEntryDto } from './dto/create-entry.dto';
 
 @Injectable()
-export class EntriesService {}
+export class EntriesService {
+  constructor(
+    @InjectModel(Entry.name) private readonly entryModel: Model<EntryDocument>,
+  ) {}
+
+  async create(dto: CreateEntryDto): Promise<Entry> {
+    return this.entryModel.create(dto);
+  }
+
+  async findAll(date?: string): Promise<Entry[]> {
+    const filter = date ? { date } : {};
+    return this.entryModel
+      .find(filter)
+      .sort({ date: -1, createdAt: -1 })
+      .exec();
+  }
+}
