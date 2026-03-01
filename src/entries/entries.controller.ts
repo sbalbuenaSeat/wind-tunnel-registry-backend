4aa598aee25b5c2424f6cf570cfa4883493a7f51
sbalbuenaSeat <saray.balbuena@code.seat>
2026-03-01 15:50:08 +0100

    feat: adding post, get, patch and delete functions to entries controller

--- src/entries/entries.controller.ts
+++ src/entries/entries.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { EntriesService } from './entries.service';
+import { CreateEntryDto } from './dto/create-entry.dto';
+import { UpdateEntryDto } from './dto/update-entry.dto';
 
 @Controller('entries')
-export class EntriesController {}
+export class EntriesController {
+  constructor(private readonly service: EntriesService) {}
+
+  @Post()
+  create(@Body() dto: CreateEntryDto) {
+    return this.service.create(dto);
+  }
+
+  @Get()
+  list(@Query('date') date?: string) {
+    return this.service.findAll(date);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateEntryDto) {
+    return this.service.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.remove(id);
+  }
+}
