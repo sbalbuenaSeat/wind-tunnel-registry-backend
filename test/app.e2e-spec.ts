86228809510f08314db6903b7afb20d993a92e7c
sbalbuenaSeat <saray.balbuena@code.seat>
2026-03-04 12:32:27 +0100

    feat: mocks e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,18 +2,34 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { getModelToken } from '@nestjs/mongoose';
+import { Entry } from '../src/entries/schemas/entry.schema';
+import { vi } from 'vitest';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
+  const mockEntryModel = {
+    find: vi.fn(),
+    create: vi.fn(),
+    exec: vi.fn(),
+  };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(getModelToken(Entry.name))
+      .useValue(mockEntryModel)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
