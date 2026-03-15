17d06b0fb5dc95523bcda4d511f813e4dfd5efc4
sbalbuenaSeat <saray.balbuena@code.seat>
2026-03-15 17:54:28 +0100

    fix: update DTOs swagger

--- src/entries/dto/import-entries.dto.ts
+++ src/entries/dto/import-entries.dto.ts
@@ -1,8 +1,13 @@
 import { IsArray, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CreateEntryDto } from './create-entry.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ImportEntriesDto {
+  @ApiProperty({
+    type: [CreateEntryDto],
+    description: 'Array of flight time entries to import',
+  })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => CreateEntryDto)
